8ecff4021209b3d5b8ac75ee8d6e2d43cfdede50
XiawPohr <paradise121329@gmail.com>
2017-04-07 16:45:59 +0800

    feature: user can see all product in the cart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,20 @@
 class OrdersController < ApplicationController
 
+  def index
+    @orders = current_cart.orders.all
+  end
+
   def create
     product = Product.find(params[:product_id])
-    order = current_cart.orders.new(product_id: product.id)
-    order.save
+    existed_order = current_cart.orders.find_by_product_id(product.id)
+    if existed_order
+      existed_order.product_amount += 1
+      existed_order.save
+    else
+      new_order = current_cart.orders.new(product_id: product.id)
+      new_order.product_amount = 1
+      new_order.save
+    end
     redirect_to root_path
   end
 
