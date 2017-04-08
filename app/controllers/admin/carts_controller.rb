ea960dd83c9fe2117b6999f51c46705aa43f5b9e
LYC <newvry@hotmail.com>
2017-04-08 15:18:11 +0800

    feature: admin can update user’s order

--- app/controllers/admin/carts_controller.rb
+++ app/controllers/admin/carts_controller.rb
@@ -8,10 +8,35 @@ class Admin::CartsController < ApplicationController
     @cart = Cart.find(params[:id])
   end
 
+  def edit
+    @cart = Cart.find(params[:id])
+  end
+
+  def update
+    @cart = Cart.find(params[:id])
+    if @cart.update(cart_params)
+      redirect_to admin_cart_path(@cart)
+    else
+      render :back
+    end
+  end
+
   def destroy
     @cart = Cart.find(params[:id])
     @cart.destroy
     redirect_to admin_carts_path
+  end
+
+  private
+
+  def cart_params
+    params.require(:cart).permit(:total_price,
+                                 :payment,
+                                 :order_status,
+                                 :money_status,
+                                 :receiver,
+                                 :phone_number,
+                                 :address)
   end
 
 end
