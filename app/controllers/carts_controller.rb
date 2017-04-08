751f26881d921855679d1785826960601bc41ff1
XiawPohr <paradise121329@gmail.com>
2017-04-08 12:23:58 +0800

    controller: casts#create

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,5 +4,22 @@ class CartsController < ApplicationController
   def new
     @cart = current_cart
   end
+
+  def create
+    current_cart.update(cart_params)
+    current_cart.user = current_user
+    if current_cart.save
+      @cart = nil
+      session[:cart_id] = nil
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def cart_params
+      params.require(:cart).permit(:receiver, :phone_number, :address, :payment)
+    end
   
 end
